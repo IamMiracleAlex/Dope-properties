e33381d7e85cde8dc56d8344f2cdb8bd01995316
Mirakul1 <collinsalex50@gmail.com>
2019-12-21 15:58:23 +0100

    added some text on about page

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#6rp*lawtwf#a9jzkxm6qqdclg0hlg5va0!g5el*64w28g-@8!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -85,23 +85,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydb',
-	    'USER': 'postgres',
-	    'PASSWORD': '12345678',
-	    'HOST': 'database-1.ceyu3tteanxd.us-east-2.rds.amazonaws.com',
-	    'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'mydb',
+# 	    'USER': 'postgres',
+# 	    'PASSWORD': '12345678',
+# 	    'HOST': 'database-1.ceyu3tteanxd.us-east-2.rds.amazonaws.com',
+# 	    'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
