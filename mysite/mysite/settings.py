c4f930c47d612eff7d9132998648ba96a053bed9
Mirakul1 <collinsalex50@gmail.com>
2019-12-22 21:57:18 +0100

    added title block to admin page, link to admin logo

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -85,6 +85,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
