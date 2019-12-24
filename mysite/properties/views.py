4672ee84f2854a26a8a3912fb9d6bb13a9a7d5dc
Mirakul1 <collinsalex50@gmail.com>
2019-12-24 17:23:20 +0100

    changed the search logic to post

--- mysite/properties/views.py
+++ mysite/properties/views.py
@@ -30,33 +30,33 @@ def search(request):
     query_list = Property.objects.order_by('-list_date') 
 
     # keywords
-    if 'keywords' in request.GET:   # checks for keyword search
-        keywords = request.GET['keywords']  
+    if 'keywords' in request.POST:   # checks for keyword search
+        keywords = request.POST['keywords']  
         if keywords:     # makes sure keyword is not an empty string
             # searches if the keyword is contained in the field description (case insensitive)
             query_list = query_list.filter(description__icontains=keywords) 
 
     # city
-    if 'city' in request.GET:
-        city = request.GET['city']
+    if 'city' in request.POST:
+        city = request.POST['city']
         if city:
             query_list = query_list.filter(city__iexact=city) # case insensitive exact search
 
     # state
-    if 'state' in request.GET:
-        state = request.GET['state']
+    if 'state' in request.POST:
+        state = request.POST['state']
         if state:
             query_list = query_list.filter(state__iexact=state)
 
     # bedrooms
-    if 'bedrooms' in request.GET:
-        bedrooms = request.GET['bedrooms']
+    if 'bedrooms' in request.POST:
+        bedrooms = request.POST['bedrooms']
         if bedrooms:
             query_list = query_list.filter(bedrooms__lte=bedrooms)  # less than or equal to search 
 
     # price
-    if 'price' in request.GET:
-        price = request.GET['price']
+    if 'price' in request.POST:
+        price = request.POST['price']
         if price:
             query_list = query_list.filter(price__lte=price)             
 
@@ -65,7 +65,7 @@ def search(request):
         'bedroom_choices': bedroom_choices,
         'price_choices': price_choices,
         'properties': query_list,
-        'values': request.GET
+        'values': request.POST
     }
     return render(request, 'properties/search.html', context) 
 
